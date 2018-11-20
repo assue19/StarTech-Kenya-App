d6dc49282abc0e1bad0cc574cb42e0509a626533
Student <assumpterkimeu16@gmail.com>
2018-11-20 07:55:16 -0800

    baseapp

---
+++ app/components/Login.js
@@ -0,0 +1,236 @@
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  AsyncStorage,
+  Image,
+  TextInput,
+  StyleSheet, // CSS-like styles
+  Text, // Renders text
+  View,
+  Modal,
+  ActivityIndicator,
+  ScrollView
+
+} from "react-native";
+
+import { StackNavigator } from "react-navigation";
+import Loader from "./Loader";
+import renderIf from "./util/renderIf";
+//import Spinner from "react-native-loading-spinner-overlay";
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      email: "",
+      password: ""
+    };
+  }
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: "#16a085",
+      elevation: null
+    },
+    headerTitleStyle: {
+      textAlign: "left",
+      alignSelf: "center",
+      flex: 1
+    },
+    headerTintColor: "#fff"
+  };
+
+
+  onLoginPress = () => {
+    const { email, password } = this.state;
+
+    this.setState({
+      loading: true
+    });
+
+    fetch("https://68c0ff0a.ngrok.io/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        // If server response message same as Data Matched
+        if (responseJson.statusCode === 200) {
+
+          this.setState({
+            loading: false,
+            email: "false",
+            password: "password"
+          });
+          //Then open Profile activity and send user email to profile activity.
+          this.props.navigation.navigate("HomeScreen");
+        } else {
+          Alert.alert(responseJson.message);
+        }
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+        console.error(error);
+      });
+  };
+
+  render() {
+    return (
+       
+      <View style={styles.container}>
+      <ScrollView>
+        <View behavior="padding" style={styles.container}>
+          <View style={styles.logoContainer}>
+            <Loader loading={this.state.loading} />
+            <Image style={styles.logo} source={require("./logo2.png")} />
+            <Text style={styles.subtext}>StarTech Kenya</Text>
+          </View>
+
+          <KeyboardAvoidingView style={styles.keyboard}>
+            <View style={styles.window}>
+              <TextInput
+                placeholder="Enter Username"
+                placeholderTextColor="rgba(255,255,255,0.7)"
+                returnKeyType="next"
+                onSubmitEditing={() => this.passwordInput.focus()}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={this.state.email}
+                style={styles.input}
+                onChangeText={email => this.setState({ email })}
+              />
+            </View>
+
+            <View style={styles.window}>
+              <TextInput
+                placeholder="Enter Password"
+                placeholderTextColor="rgba(255,255,255,0.7)"
+                returnKeyType="go"
+                secureTextEntry
+                ref={input => (this.passwordInput = input)}
+                value={this.state.password}
+                style={styles.input}
+                onChangeText={password => this.setState({ password })}
+              />
+            </View>
+
+            <TouchableOpacity
+              style={styles.buttonContainer}
+              onPress={
+                //this.onLoginPress.bind(this)
+                () => this.props.navigation.navigate("HomeScreen")
+              }
+            >
+              <Text style={styles.buttonText}>LOGIN</Text>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        </View>
+
+        <View style={styles.horizontalContainer}>
+
+            <TouchableOpacity style={styles.button}
+            onPress={() => this.props.navigation.navigate("Register")}>
+              <Text
+                style={styles.buttonText}
+                
+                title="Sign up"
+              >
+                Sign up
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} 
+            onPress={() => this.props.navigation.navigate("ForgetPassword")}>
+              <Text
+                style={styles.buttonText}
+              
+                title="Forget Password"
+              >
+                Forget Password
+              </Text>
+            </TouchableOpacity>
+        </View>
+
+    </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2f39f2"
+
+  },
+  horizontalContainer:{
+    flex: 1,
+    flexDirection:"row"
+  },
+  logoContainer: {
+    alignItems: "center",
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingTop:15
+  },
+  logo: {
+    width: 200,
+    height: 150
+  },
+  subtext: {
+    color: "#ffffff",
+    width: "100%",
+    textAlign: "center",
+    fontSize: 35,
+    fontWeight: "bold",
+    marginTop: 20,
+    opacity: 0.8
+  },
+  keyboard: {
+    margin: 20,
+    padding: 20,
+    alignSelf: "stretch"
+  },
+  buttonContainer: {
+    backgroundColor: "rgba(255,255,255,0.2)",
+    paddingVertical: 15
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#FFF",
+    fontWeight: "700"
+  },
+  button: {
+    paddingVertical: 15,
+    padding: 20,
+    marginBottom: 5,
+    marginTop: 5,
+    marginRight: 20,
+    // marginLeft: 20,
+    borderRadius:20
+  },
+  input: {
+    height: 40,
+    marginBottom: 10,
+    backgroundColor: "rgba(255,255,255,0.2)",
+    color: "#fff",
+    paddingHorizontal: 10
+  },
+  window: {
+    marginBottom: 15
+  }
+});
+
+AppRegistry.registerComponent("Login", () => Login);
---
+++ app/components/Register.js
@@ -0,0 +1,182 @@
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Image,
+  KeyboardAvoidingView,
+  AsyncStorage,
+  ScrollView
+} from "react-native";
+
+import { StackNavigator } from "react-navigation";
+
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      name: "",
+      password: "",
+      password_confirmation: ""
+    };
+  }
+
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: "white",
+      elevation: null,
+    },
+    headerTitleStyle: {
+      color: '#fff',
+    },
+    headerTitleStyle: {
+      textAlign:'left', 
+      alignSelf:'center',
+    },
+    headerTintColor: '#fff',
+  };
+
+  async onRegisterPress() {
+    const { email, password, name } = this.state;
+    console.log(email);
+    console.log(name);
+    console.log(password);
+    await AsyncStorage.setItem("email", email);
+    await AsyncStorage.setItem("name", name);
+    await AsyncStorage.setItem("password", password);
+    this.props.navigation.navigate("HomeScreen");
+  }
+
+  render() {
+    return (
+      <ScrollView style={{ backgroundColor: "blue"}}>
+      <View behavior="padding" style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={require("./logo2.png")} />
+        </View>
+
+        <View style={styles.containerTextFields}>
+        <KeyboardAvoidingView>
+          <TextInput
+            value={this.state.name}
+            onChangeText={name => this.setState({ name })}
+            style={styles.input}
+            placeholder="Name"
+            placeholderTextColor="rgba(255,255,255,0.7)"
+            returnKeyType="next"
+            onSubmitEditing={() => this.emailInput.focus()}
+          />
+
+          <TextInput
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+            style={styles.input}
+            placeholderTextColor="rgba(255,255,255,0.7)"
+            returnKeyType="next"
+            ref={input => (this.emailInput = input)}
+            onSubmitEditing={() => this.passwordCInput.focus()}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Email"
+          />
+          <TextInput
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            placeholderTextColor="rgba(255,255,255,0.7)"
+            ref={input => (this.passwordCInput = input)}
+            onSubmitEditing={() => this.passwordInput.focus()}
+            returnKeyType="next"
+            secureTextEntry
+          />
+
+          <TextInput
+            value={this.state.password}
+            onChangeText={password_confirmation => this.setState({ password_confirmation })}
+            style={styles.input}
+            placeholder="Confirm Password"
+            secureTextEntry={true}
+            placeholderTextColor="rgba(255,255,255,0.7)"
+            returnKeyType="go"
+            secureTextEntry
+            ref={input => (this.passwordInput = input)}
+          />
+        </KeyboardAvoidingView>
+
+        <TouchableHighlight
+          onPress={this.onRegisterPress.bind(this)}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableHighlight>
+        </View>
+      </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+  },
+  containerTextFields: {
+    paddingTop: 20
+  },
+  logoContainer: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  logo: {
+    width: 200,
+    height: 150
+  },
+  input: {
+    height: 40,
+    width: 350,
+    marginBottom: 10,
+    backgroundColor: "rgba(255,255,255,0.2)",
+    color: "#fff",
+    paddingHorizontal: 10
+  },
+  button: {
+    height: 55,
+    backgroundColor: "rgba(255,255,255,0.2)",
+    alignSelf: "stretch",
+    marginTop: 10,
+    justifyContent: "center",
+    paddingVertical: 15,
+    marginBottom: 10
+  },
+  buttonText: {
+    flex: 1,
+    width: '100%',
+    fontSize: 18,
+    alignSelf: "center",
+    textAlign: "center",
+    color: "#FFF",
+    fontWeight: "700"
+  },
+  subtext: {
+    color: "#ffffff",
+    width:'100%',
+    textAlign: "center",
+    fontSize: 35,
+    fontWeight: "bold",
+    marginTop: 20,
+    opacity: 0.8
+  }
+});
+
+AppRegistry.registerComponent("Register", () => Register);
